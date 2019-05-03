073f45f8dd145503a55d4170f1405729f4437a30
VukhuongDuy <vukhuongduy23@gmail.com>
2019-05-03 23:04:03 +0700

    1.1

--- public/javascript/handle.js
+++ public/javascript/handle.js
@@ -8,14 +8,35 @@ function clickSave(){
 }
 
 function loadText(){
-    var rad = Math.random();
-    var index = Number.parseInt(rad*3)+1
-    document.body.style.backgroundImage = "url('../img/"+index+".jpg')";
     let txt = localStorage.getItem('note') || '';
+    if(!localStorage.getItem('img'))
+        localStorage.setItem('img','https://www.besthealthmag.ca/wp-content/uploads/sites/16/2018/04/Run-club-woman-running.jpg')
+    let url = localStorage.getItem('img');
+    document.body.style.backgroundImage = "url('"+url+"')"
     document.getElementById('note').value = txt;
 }
 
 function textChange(){
     let btnSave = document.getElementById('btn-save');
     btnSave.style.backgroundColor = "#ff9900";
+}
+
+function clickBtnBackground(){
+    document.getElementById("txtUrl").style.display = 'inline'
+}
+
+function changeTxtUrl(){
+    var txtUrl = document.getElementById("txtUrl");
+    let index = txtUrl.value.indexOf('.jpg');
+    if(index>0){
+        document.body.style.backgroundImage = "url('"+txtUrl.value+"')"
+        localStorage.setItem('img',txtUrl.value);
+        txtUrl.style.display = 'none'
+    }else{
+        alert('Hãy chọn 1 ảnh jpg');
+    }
+}
+
+function clickSaveImage(){
+    let file = document.getElementById('fileImg')
 }
--- server.js
+++ server.js
@@ -12,5 +12,15 @@ server.listen(process.env.PORT||3000);
 app.get('/',function(req,res){
     res.render('home');
 })
+// var JSZip = require('jszip')
+// var zip = new JSZip();
+// var img = zip.folder("img");
+// img.file("1.jpg", imgData, {base64: true});
+ 
+// zip.generateAsync({type:"blob"}).then(function(content) {
+//     // see FileSaver.js
+//     saveAs(content, "example.zip");
+// });
 
-
+// console.log(zip)
+ 
