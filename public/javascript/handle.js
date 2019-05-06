0f50700bb3076bbbdd57d24271e8188730bb3cb8
VukhuongDuy <vukhuongduy23@gmail.com>
2019-05-06 23:38:23 +0700

    1.1

--- public/javascript/handle.js
+++ public/javascript/handle.js
@@ -9,10 +9,18 @@ function clickSave(){
 
 function loadText(){
     let txt = localStorage.getItem('note') || '';
-    if(!localStorage.getItem('img'))
+    if(!localStorage.getItem('background')){
+        localStorage.setItem('background','yes')
+    }
+
+    if(localStorage.getItem('background') === 'yes'){
         localStorage.setItem('img','https://www.besthealthmag.ca/wp-content/uploads/sites/16/2018/04/Run-club-woman-running.jpg')
-    let url = localStorage.getItem('img');
-    document.body.style.backgroundImage = "url('"+url+"')"
+        let url = localStorage.getItem('img');
+        document.body.style.backgroundImage = "url('"+url+"')"
+    }
+    else{
+        document.body.style.backgroundColor = localStorage.getItem('background');
+    }
     document.getElementById('note').value = txt;
 }
 
@@ -27,14 +35,20 @@ function clickBtnBackground(){
 
 function changeTxtUrl(){
     var txtUrl = document.getElementById("txtUrl");
+    if(localStorage.getItem('background') === 'yes'){
     let index1 = txtUrl.value.indexOf('.jpg');
-    let index2 = txtUrl.value.indexOf('.png')
-    if(index>0 || index2>0){
+    let index2 = txtUrl.value.indexOf('.png');
+    if(index1>0 || index2>0){
         document.body.style.backgroundImage = "url('"+txtUrl.value+"')"
         localStorage.setItem('img',txtUrl.value);
         txtUrl.style.display = 'none'
     }else{
         alert('Hãy chọn 1 ảnh jpg');
+    }
+    }
+    else {
+        document.body.style.backgroundColor = txtUrl.value
+        localStorage.setItem('background',txtUrl.value);
     }
 }
 
